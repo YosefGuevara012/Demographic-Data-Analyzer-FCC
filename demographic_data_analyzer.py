62245ef16250324515a4c2984e543dceaf94bf28
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-02-10 13:07:20 -0500

    Bachelors and average men age added

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,17 +3,18 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = None
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df[df["sex"] == "Male"]["age"].mean()
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
-
+    bachelors = df[df["education"] == "Bachelors"]
+    percentage_bachelors = float(len(bachelors)) / float(len(df))*100
+    
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
